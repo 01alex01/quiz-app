a2c2f9147bf11631151c38f59371f8174abb7655
alex atolagbe <01atolagbealex01@gmail.com>
2024-03-19 11:23:20 +0100

    writing the question and answers

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    questions: "what is the largest animal on earth",
+    anwsers: [
+      { text: "shark", correct: false },
+      { text: "blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    questions: "what is the smallest country in the world",
+    anwsers: [
+      { text: "Vantica city", correct: true },
+      { text: "Bhutan", correct: false },
+      { text: "Nepal", correct: false },
+      { text: "shri Lanka", correct: false },
+    ],
+  },
+  {
+    questions: "what is the largest desert in the world",
+    anwsers: [
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "sahara", correct: false },
+      { text: "Antarctica", correct: true },
+    ],
+  },
+  {
+    questions: "what is the smallest continent in the world",
+    anwsers: [
+      { text: "Africa", correct: false },
+      { text: "Asia", correct: true },
+      { text: "Australia", correct: true },
+      { text: "Arctic", correct: false },
+    ],
+  },
+];
