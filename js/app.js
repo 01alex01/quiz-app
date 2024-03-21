eab3e325cf078df0a5bcc9d98a513d702e08d976
alex atolagbe <01atolagbealex01@gmail.com>
2024-03-21 01:15:06 +0100

    still working on the java script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const questions = [
   {
-    questions: "what is the largest animal on earth",
+    question: "what is the largest animal on earth",
     anwsers: [
       { text: "shark", correct: false },
       { text: "blue whale", correct: true },
@@ -9,7 +9,7 @@ const questions = [
     ],
   },
   {
-    questions: "what is the smallest country in the world",
+    question: "what is the smallest country in the world",
     anwsers: [
       { text: "Vantica city", correct: true },
       { text: "Bhutan", correct: false },
@@ -18,7 +18,7 @@ const questions = [
     ],
   },
   {
-    questions: "what is the largest desert in the world",
+    question: "what is the largest desert in the world",
     anwsers: [
       { text: "Kalahari", correct: false },
       { text: "Gobi", correct: false },
@@ -27,7 +27,7 @@ const questions = [
     ],
   },
   {
-    questions: "what is the smallest continent in the world",
+    question: "what is the smallest continent in the world",
     anwsers: [
       { text: "Africa", correct: false },
       { text: "Asia", correct: true },
@@ -36,3 +36,29 @@ const questions = [
     ],
   },
 ];
+const questionsElemennt = document.getElementById("question");
+const anwserButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+function showQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionsElemennt.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.anwsers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    anwserButton.appendChild(button);
+  });
+}
+startQuiz();
